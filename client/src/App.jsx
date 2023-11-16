152503f49a3d0068144e402f26e379e9b0618a9b
Biswajit Swain <biswajitswain552@gmail.com>
2023-11-16 23:27:28 +0530

    Header add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, About, SignIn, SignUp, Profile } from "./pages";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
