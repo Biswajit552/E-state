e4eb577c23d3e856ce5e4749510db75ddc78aae2
Biswajit Swain <biswajitswain552@gmail.com>
2023-11-17 12:16:15 +0530

    create and set of api and controller routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Biswa-Estate")
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000 ");
 });
+
+app.use("/api/user", userRouter);
