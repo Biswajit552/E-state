e17d183ab2cd30820bc9831116eae3a1d624a47e
Biswajit Swain <biswajitswain552@gmail.com>
2023-11-18 14:09:10 +0530

    firebase connect complete

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://th.bing.com/th/id/OIP.MYwdjrgFU0JwL6ahVIdgZwHaH_?w=184&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
   },
   { timestamps: true }
 );
