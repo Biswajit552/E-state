46b5cef23a34240088223aac3469c4fe1156f1b6
Biswajit Swain <biswajitswain552@gmail.com>
2023-11-23 11:28:44 +0530

    just change index.js severside

--- api/index.js
+++ api/index.js
@@ -11,18 +11,22 @@ dotenv.config();
 mongoose
   .connect("mongodb://127.0.0.1:27017/Biswa-Estate")
   .then(() => {
-    console.log("connected to mongodb");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
+
 const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000 ");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
@@ -30,13 +34,14 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-
   return res.status(statusCode).json({
     success: false,
     statusCode,
