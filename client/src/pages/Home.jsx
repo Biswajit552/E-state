e3861b18796671e92da2be9fa92b4c842e5129d7
Biswajit Swain <biswajitswain552@gmail.com>
2024-04-27 13:35:00 +0530

    rect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch("/api/listing/get?type=rent&limit=4");
+        const res = await fetch("/api/listing/get?type=rent&limit=6");
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
